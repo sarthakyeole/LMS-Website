77098ac0d6ff22e98c0da135b4e608478aab6455
sarthakyeole <yeolesarthakdev@gmail.com>
2025-05-06 15:21:35 +0530

    Initial commits

---
+++ client/src/components/student/Companies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Companies = () => {
+  return (
+    <div>
+      <h1>Comapnies</h1>
+    </div>
+  )
+}
+
+export default Companies
---
+++ client/src/pages/educator/MyCourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyCources = () => {
+  return (
+    <div>
+      <h1>My Courses</h1>
+    </div>
+  )
+}
+
+export default MyCources
